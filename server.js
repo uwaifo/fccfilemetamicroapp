ce6d8ac1f6c63f7f6eaf9f576dcde9dc84189dae
Glitch (fccfilemetamicroapp) <none>
2019-08-07 14:53:57 +0000

    💆🙇 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer')
+const bodyParser = require('body-parser');
+const uploader = multer({dest: './public'})
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.route('/api/fileanalyse')
+.post(uploader.single('upfile'),(req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
